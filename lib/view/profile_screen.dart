5f7e880c087a993db683e6491dbad8c52a83956d
Amaldas467 <amaldas467@gmail.com>
2025-01-14 13:00:25 +0530

    profilescreen updated

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:skincare_app/utilities/color_constants.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -12,6 +13,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
   TextEditingController dobController = TextEditingController();
   String? ageError;
   final FocusNode dobFocusNode = FocusNode();
+
+  String imageUrl =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy5zKoI_m0hy7V1711x_xYAGJesoMf7jwyhQ&s';
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? selectedDate = await showDatePicker(
@@ -49,6 +53,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
@@ -58,61 +65,102 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 minHeight: constraints.maxHeight,
               ),
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
+                padding: EdgeInsets.all(screenWidth * 0.04),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Profile Details',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 20),
+                    Center(
+                      child: Text('Profile Details',
+                          style: TextStyle(
+                              fontSize: screenWidth * 0.05,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // CircleAvatar with Border Decoration
+                    Center(
+                      child: CircleAvatar(
+                        radius: screenWidth * 0.20,
+                        backgroundImage: NetworkImage(imageUrl),
+                        child: imageUrl == null
+                            ? Icon(Icons.person, size: screenWidth * 0.15)
+                            : null,
+                        backgroundColor: Colors.grey[200],
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.025),
+
+                    // Full Name TextField with Custom Decoration
                     TextField(
                       decoration: InputDecoration(
                         labelText: 'Full name',
-                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(fontSize: screenWidth * 0.04),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blueAccent),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
                       ),
                     ),
-                    SizedBox(height: 20),
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // Mobile Number TextField with Custom Decoration
                     TextField(
                       decoration: InputDecoration(
                         labelText: 'Mobile number',
-                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(fontSize: screenWidth * 0.04),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blueAccent),
+                        ),
                         prefixIcon: Padding(
                           padding: const EdgeInsets.only(left: 10.0, top: 13),
                           child: Text(
                             '+91 ',
                             style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
+                                fontSize: screenWidth * 0.04,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
                         suffixIcon: TextButton(
                           onPressed: () {
                             // Verify action
                           },
-                          child: Text('Verify'),
+                          child: Text('Verify',
+                              style: TextStyle(color: Colors.blue)),
                         ),
                       ),
                     ),
-                    SizedBox(height: 20),
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // Email TextField with Check Icon
                     TextField(
                       decoration: InputDecoration(
                         labelText: 'Email address',
-                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(fontSize: screenWidth * 0.04),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blueAccent),
+                        ),
                         suffixIcon:
                             Icon(Icons.check_circle, color: Colors.green),
                       ),
                     ),
-                    SizedBox(height: 20),
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // Date of Birth TextField with Calendar Icon
                     TextField(
                       controller: dobController,
                       decoration: InputDecoration(
                         labelText: 'Date of birth',
+                        labelStyle: TextStyle(fontSize: screenWidth * 0.04),
                         border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              //color: dobFocusNode.hasFocus
-                              //    ? Colors.green
-                              //    : Colors.grey, // Green border when focused
-                              ),
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              BorderSide(color: ColorConstants.buttonColor),
                         ),
                         suffixIcon: IconButton(
                           icon: Icon(Icons.calendar_today),
@@ -124,10 +172,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ),
                       readOnly: true,
                     ),
-                    SizedBox(height: 20),
-                    Text('GENDER', style: TextStyle(fontSize: 16)),
-                    SizedBox(height: 10),
-                    // Dropdown for Gender
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // Gender Dropdown Button
+                    Text('GENDER',
+                        style: TextStyle(fontSize: screenWidth * 0.04)),
+                    SizedBox(height: screenHeight * 0.01),
                     DropdownButtonFormField<String>(
                       value: selectedGender,
                       onChanged: (String? newValue) {
@@ -137,7 +187,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       },
                       decoration: InputDecoration(
                         labelText: 'Select Gender',
-                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(fontSize: screenWidth * 0.04),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blueAccent),
+                        ),
                       ),
                       items: ['Male', 'Female', 'Undisclosed']
                           .map((String gender) {
@@ -147,10 +201,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         );
                       }).toList(),
                     ),
-                    SizedBox(height: 20),
-                    Text('MARITAL STATUS', style: TextStyle(fontSize: 16)),
-                    SizedBox(height: 10),
-                    // Dropdown for Marital Status
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // Marital Status Dropdown Button
+                    Text('MARITAL STATUS',
+                        style: TextStyle(fontSize: screenWidth * 0.04)),
+                    SizedBox(height: screenHeight * 0.01),
                     DropdownButtonFormField<String>(
                       value: selectedMaritalStatus,
                       onChanged: (String? newValue) {
@@ -160,7 +216,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       },
                       decoration: InputDecoration(
                         labelText: 'Select Marital Status',
-                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(fontSize: screenWidth * 0.04),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blueAccent),
+                        ),
                       ),
                       items: ['Married', 'Unmarried', 'Undisclosed']
                           .map((String status) {
@@ -170,23 +230,38 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         );
                       }).toList(),
                     ),
-                    SizedBox(height: 20),
-                    SizedBox(height: 5),
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // Save Button with Decoration
                     Center(
                       child: OutlinedButton(
                         onPressed: () {},
                         child: Text('Save'),
                         style: OutlinedButton.styleFrom(
-                          minimumSize: Size(double.infinity, 50),
+                          minimumSize:
+                              Size(double.infinity, screenHeight * 0.065),
+                          side: BorderSide(color: ColorConstants.buttonColor),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
                       ),
                     ),
-                    SizedBox(height: 15),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: Text('Logout'),
-                      style: OutlinedButton.styleFrom(
-                        minimumSize: Size(double.infinity, 50),
+                    SizedBox(height: screenHeight * 0.02),
+
+                    // Logout Button with Decoration
+                    Center(
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        child: Text('Logout'),
+                        style: OutlinedButton.styleFrom(
+                          minimumSize:
+                              Size(double.infinity, screenHeight * 0.065),
+                          side: BorderSide(color: ColorConstants.buttonColor),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
                       ),
                     ),
                   ],
